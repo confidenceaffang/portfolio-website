4314fce6614321c253f92d8ea2c18463f00e8111
confidenceaffang <coaffa01@wsc.edu>
2024-10-27 01:15:19 -0500

    changed background

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,9 @@ const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-slate-300 selection:text-slate-900">   
       <div className="fixed inset-0 -z-10 h-full w-full">
-        <div className="absolute inset-0 h-full w-full px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
-      </div>
+      <div className="relative size-full bg-slate-950">
+  <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]" />
+</div></div>
       <div className="container mx-auto my-8">
         <Navbar />
         <Body />
